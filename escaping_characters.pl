32a5dbbe943594e55b828f141983aa93bbf93a44
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-03-02 19:37:42 +0100

    how escape chars work
    There are many escaping characters in Perl, and these are some examples on how to use them.

---
+++ escaping_characters.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+$my_var= "5 is different from \"five\"\n";
+#print $my_var;
+
+print "Do you hear the bells?\a\n";
+
+print "Do you see the ga\t\tp?\n";
+
+print "The odd \uone out\n";
+
+print "MY \lNEW GUITAR!\n";
+
+print "the next \Ucase\n";
+
+print "THE LAST \LCASE\n";
+
+print "\Q!@^&*_+\n";
