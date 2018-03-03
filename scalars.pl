8ea78c6f7d83e25ba864dcc047f4051b8c303858
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-03-03 20:28:26 +0100

    Introduction to scalars
    For me, I'll just call it datatypes

---
+++ scalars.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+$rank=1;
+$name="Novak";
+$tot_money=55555555.55;
+$my_oct=0167;
+$my_hex=0xabe;
+
+print "Name: $name\n";
+print "Rank: $rank\n";
+print "Total Prize Money: $tot_money\n";
+
+print "My Octal Number: $my_oct\n"; #119
+print "My Hexadecimal Number: $my_hex\n"; #2750
