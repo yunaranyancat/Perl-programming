0e3a8b0edea9c978af06b58ca78a0b899e096942
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-03-03 20:29:45 +0100

    Simple array operations
    The use of .. and size of the scalar (array type)

---
+++ arrays_ops.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+@ranks=(1..10);
+@alphabets=(a..z);
+
+print "All ranks: @ranks\n";
+print "All Letters: @alphabets\n";
+
+$size=@alphabets;
+
+print "Size of Letters array: $size\n";
