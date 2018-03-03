b6e6bb4ffae6553a544414611eb32cda2c9d3412
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-03-03 20:28:02 +0100

    Difference between double and single quote(s)

---
+++ quotes.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+#This is a comment and it will just be
+#ignored by perl
+
+print "Hello World\n";
+print 'Hello World\n';
+
+$my_var=20;
+
+print "\nValue of my variable: $my_var\n";
+print 'Value of my variable: $my_var\n';
